938b85ad230ccf827c6c7362f54c3687d112b44a
Diego Hernandes <diegowalhernandes@gmail.com>
2023-08-25 20:08:43 -0300

    :up: Init hero section version web

---
+++ script.js
@@ -0,0 +1,9 @@
+var tipe = "https://cdn.jsdelivr.net/npm/typed.js@2.0.12"
+let typed = new Typed('.animate', {
+    strings: [
+        "SOFTWARE ENGINEER, FRONT END.",
+    ],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true
+});
